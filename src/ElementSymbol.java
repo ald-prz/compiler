217124d14964fa1e108d266c0e3a6d2eb784c5c1
ElderMayday <galdarsar@gmail.com>
2016-12-03 02:52:34 +0100

    Add stack element classes

---
+++ src/ElementSymbol.java
@@ -0,0 +1,14 @@
+
+public class ElementSymbol extends Element {
+
+	protected Symbol symbol;
+	
+	public Symbol getSymbol() {
+		return symbol;
+	}
+
+	public ElementSymbol(Symbol symbol)
+	{
+		this.symbol = symbol;
+	}
+}
---
+++ src/ElementVariable.java
@@ -0,0 +1,14 @@
+
+public class ElementVariable extends Element {
+
+	protected EnumVariable variable;
+	
+	public EnumVariable getVariable() {
+		return variable;
+	}
+
+	public ElementVariable(EnumVariable variable)
+	{
+		this.variable = variable;
+	}	
+}
